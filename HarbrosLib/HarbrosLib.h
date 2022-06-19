a90765ed89d4377b57438f52aade599ea79fb783
Valeriya Ovseychuk <v.ovseychuk@appkode.ru>
2022-06-19 14:27:50 +0300

    Initial Commit

---
+++ HarbrosLib/HarbrosLib.h
@@ -0,0 +1,18 @@
+//
+//  HarbrosLib.h
+//  HarbrosLib
+//
+//  Created by Валерия Овсейчук on 19.06.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HarbrosLib.
+FOUNDATION_EXPORT double HarbrosLibVersionNumber;
+
+//! Project version string for HarbrosLib.
+FOUNDATION_EXPORT const unsigned char HarbrosLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarbrosLib/PublicHeader.h>
+
+
