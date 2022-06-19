8aacb2ee977539214b39878e7cddf4ba636d4707
Valeriya Ovseychuk <v.ovseychuk@appkode.ru>
2022-06-19 14:33:57 +0300

    Added lib file

---
+++ HarbrosLib/HarbrosLib.swift
@@ -0,0 +1,16 @@
+//
+//  HarbrosLib.swift
+//  HarbrosLib
+//
+//  Created by Валерия Овсейчук on 19.06.22.
+//
+
+public final class HarbrosLib {
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
