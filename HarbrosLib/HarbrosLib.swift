82818aad3f332aad8be0e632e5a930631da26647
Valeriya Ovseychuk <v.ovseychuk@appkode.ru>
2022-06-19 14:52:18 +0300

    fix

--- HarbrosLib/HarbrosLib.swift
+++ HarbrosLib/HarbrosLib.swift
@@ -5,12 +5,12 @@
 //  Created by Валерия Овсейчук on 19.06.22.
 //
 
-public final class HarbrosLib {
-    public func add(a: Int, b: Int) -> Int {
+open class HarbrosLib {
+    open func add(a: Int, b: Int) -> Int {
         return a + b
     }
     
-    public func sub(a: Int, b: Int) -> Int {
+    open func sub(a: Int, b: Int) -> Int {
         return a - b
     }
 }
